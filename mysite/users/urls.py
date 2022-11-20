00ef517976a899a3d5637890c65efbd07ae808c1
brahim <brahim.boussaa1998@gmail.com>
2022-11-20 14:38:11 +0100

    fixing some bugs

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login/',LoginView.as_view(template_name='users/login.html',redirect_authenticated_user=True),name='login-page'),
     path('logout/',LogoutView.as_view(),name='logout-page'),
     path('profile/',views.profile,name='profile-page'),
-    path('profile/<int:pk>')
+    
 ]
